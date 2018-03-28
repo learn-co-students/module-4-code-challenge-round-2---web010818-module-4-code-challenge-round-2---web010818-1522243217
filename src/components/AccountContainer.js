49277a39aa4978e22147fd432b8dfd4ab358447f
github username <github email address>
2018-03-28 10:21:55 -0400

    code challenge

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,32 +1,44 @@
-import React, { Component } from 'react'
-import TransactionsList from './TransactionsList'
-import Search from './Search'
-import {transactions} from '../transactionsData'
+import React, { Component } from "react";
+import TransactionsList from "./TransactionsList";
+import Search from "./Search";
+import { transactions } from "../transactionsData";
 
 class AccountContainer extends Component {
+  state = {
+    transactions: [],
+    input: ""
+  };
 
-  constructor() {
-    super()
-
-    // get a default state working with the data imported from TransactionsData
-    // use this to get the functionality working
-    // then replace the default transactions with a call to the API
-
-  }
+  componentDidMount = () => {
+    this.setState({
+      transactions: transactions
+    });
+  };
 
   handleChange(event) {
-    // your code here
+    this.setState({
+      input: event.target.value
+    });
   }
 
-  render() {
+  filterTransaction = () => {
+    let allTransactions = [...this.state.transactions];
+    let filterTransactions = allTransactions.filter(
+      t =>
+        t.description.toLowerCase().includes(this.state.input) ||
+        t.category.toLowerCase().includes(this.state.input)
+    );
+    return filterTransactions;
+  };
 
+  render() {
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} input={this.state.input} />
+        <TransactionsList transactions={this.filterTransaction()} />
       </div>
-    )
+    );
   }
 }
 
-export default AccountContainer
+export default AccountContainer;
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,38 +1,42 @@
-import React from 'react'
+import React from "react";
+import Transaction from "./Transaction";
 
-const TransactionsList = () => {
-
+const TransactionsList = props => {
   return (
     <table className="ui celled striped padded table">
       <tbody>
         <tr>
           <th>
-            <h3 className="ui center aligned header">
-              Posted At
-            </h3>
+            <h3 className="ui center aligned header">Posted At</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
-              Description
-            </h3>
+            <h3 className="ui center aligned header">Description</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
-              Category
-            </h3>
+            <h3 className="ui center aligned header">Category</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
-              Amount
-            </h3>
+            <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
 
-        {"... your code here..."}
-
+        {props.transactions.map(transaction => {
+          const posted = transaction.posted_at;
+          const description = transaction.description;
+          const category = transaction.category;
+          const amount = transaction.amount;
+          return (
+            <Transaction
+              posted={posted}
+              description={description}
+              category={category}
+              amount={amount}
+            />
+          );
+        })}
       </tbody>
     </table>
-  )
-}
+  );
+};
 
-export default TransactionsList
+export default TransactionsList;
